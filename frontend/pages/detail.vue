1d9be2741e48279bd46e259f97ec774431c6e58c
Yamato Ashikaga <a.yamato.so.ri.00@gmail.com>
2018-12-08 00:45:52 +0900

    feat:  HackMD用のテキストコピー機能実装

--- frontend/pages/detail.vue
+++ frontend/pages/detail.vue
@@ -17,6 +17,18 @@
             開催日： {{ event.event_date }}<br>
             参加者人数：{{ event.participants }} 人
           </div>
+          <div slot="footer">
+            <vs-row vs-justify="flex-end">
+              <vs-button 
+                :class="'clipcopy'" 
+                :data-clipboard-text="createTextForHackMD()"
+                type="gradient"
+                color="success" 
+                icon="description">
+                クリップボードにコピー
+              </vs-button>
+            </vs-row>
+          </div>
         </vs-card>
       </vs-col>
     </vs-row>
@@ -51,22 +63,59 @@
 </template>
 
 <script>
+import clipboard from 'clipboard'
+
 export default {
   transition: 'slide-left',
   data: () => ({
     activeLoading: true,
     event: {},
-    frames: []
+    frames: [],
+    clipBoard: {}
   }),
   mounted: async function() {
+    let vm = this
+    //setup for clipboard
+    this.clipBoard = new clipboard('.clipcopy')
+    this.clipBoard.on('success', function(e) {
+      e.clearSelection()
+
+      vm.$vs.notify({
+        title: 'copied',
+        text: 'copied for HackMD.'
+      })
+    })
+    this.clipBoard.on('error', function(e) {})
+
     this.$vs.loading()
     const data = await this.$axios.$get('/events/' + this.$route.query.id)
     this.event = data.event
     this.frames = data.frames
     this.$vs.loading.close()
+  },
+  methods: {
+    createTextForHackMD: function() {
+      let mdText = this.event.title + '\n\n'
+
+      mdText =
+        mdText +
+        '①PHP歴、②話したいこと/聞きたいこと、③簡単な自己紹介（オプション）'
+
+      for (let frame of this.frames) {
+        mdText = mdText + '\n\n## ' + frame.name + '\n'
+        for (let member of frame.members) {
+          mdText = mdText + '* ' + member.name + '\n'
+        }
+      }
+
+      return mdText
+    }
   }
 }
 </script>
 
 <style>
+#for-hackmd-text {
+  display: none;
+}
 </style>
